bfddae3541eb3fc26544b7946a5bff6f42bb98d9
RussFenenga <russfenenga@gmail.com>
2017-03-07 10:19:08 -0800

    Added iOS App with Firebase
    First commit for app.

---
+++ iOSApp/EventHype/EventHype/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EventHype
+//
+//  Created by Russ Fenenga on 3/5/17.
+//  Copyright © 2017 com.RussellFenenga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSApp/EventHype/EventHype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EventHype
+//
+//  Created by Russ Fenenga on 3/5/17.
+//  Copyright © 2017 com.RussellFenenga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
