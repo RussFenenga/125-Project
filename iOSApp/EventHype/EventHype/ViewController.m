774aec2603db5719ad8d47f768c2fc6686ffd170
RussFenenga <russfenenga@gmail.com>
2017-03-07 12:17:35 -0800

    Map View and Nav Controller
    Map view centers on user location and is embedded in a navigation
    controller.

--- iOSApp/EventHype/EventHype/ViewController.m
+++ iOSApp/EventHype/EventHype/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+@import MapKit;
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *eventMapView;
+@property CLLocationManager *locationManager;
 
 @end
 
@@ -17,6 +22,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    self.locationManager.distanceFilter = kCLDistanceFilterNone;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+        [self.locationManager requestWhenInUseAuthorization];
+    
+    [self.locationManager startUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    CLLocation *location = locations[0];
+    MKCoordinateRegion coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 500,500);
+    [self.eventMapView setRegion:coordinateRegion];
+    
 }
 
 
