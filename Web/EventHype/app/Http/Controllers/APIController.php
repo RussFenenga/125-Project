0a26f79518936af3f8d1d11eb5c0ba5c63dbd1ee
ArturGrigio <fsastudent@yahoo.com>
2017-03-16 16:57:33 -0700

    changed the post event to match string to date and added a response

--- Web/EventHype/app/Http/Controllers/APIController.php
+++ Web/EventHype/app/Http/Controllers/APIController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Event;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
@@ -37,10 +38,15 @@ class APIController extends Controller
 		    'source'=> $request->has('source') ? $request->get('source') : '',
 		    'price'=> $request->has('price') ? $request->get('price') : '',
 		    'logo'=> $request->has('logo') ? $request->get('logo') : '',
-		    'start_date'=> $request->has('start_date') ? $request->get('start_date') : '',
-		    'start_time'=> $request->has('start_time') ? $request->get('start_time') : '',
-		    'end_date'=> $request->has('end_date') ? $request->get('end_date') : '',
-		    'end_time'=> $request->has('end_time') ? $request->get('end_time') : ''
+		    'start_date'=> $request->has('start_date') ? Carbon::parse($request->get('start_date'))->toDateString() : Carbon::now()->toDateString(),
+		    'start_time'=> $request->has('start_time') ? Carbon::parse($request->get('start_time'))->toTimeString() : Carbon::now()->toTimeString(),
+		    'end_date'=> $request->has('end_date') ? Carbon::parse($request->get('end_date'))->toDateString() : Carbon::now()->toDateString(),
+		    'end_time'=> $request->has('end_time') ? Carbon::parse($request->get('end_time'))->toTimeString() : Carbon::now()->toTimeString()
+	    ));
+
+	    return json_encode(array(
+	    	'status'=>200,
+		    'response'=>'Event Saved.'
 	    ));
     }
 }
