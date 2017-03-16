ecee78c71735fd41f996e3ff084a7118b59181a0
ArturGrigio <fsastudent@yahoo.com>
2017-03-16 16:44:52 -0700

    change the event ordering to DESC on created_at

--- Web/EventHype/app/Http/Controllers/PageController.php
+++ Web/EventHype/app/Http/Controllers/PageController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function showMain(Request $request) {
-    	return view('welcome')->with(['events'=>Event::all()]);
+    	return view('welcome')->with(['events'=>Event::orderBy('created_at', 'DESC')->get()]);
     }
 }
